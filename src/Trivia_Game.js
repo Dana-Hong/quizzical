7cad73393d26cb57f9b04e69608324e2c7722471
Dana <dana.hong@hotmail.com>
2022-08-18 18:18:51 -0400

    Add API call function when Start quiz button clicked

--- src/Trivia_Game.js
+++ src/Trivia_Game.js
@@ -4,12 +4,24 @@ import { Trivia } from "./Trivia";
 
 export const Trivia_Game = () => {
     let [gameStart, setgameStart] = React.useState(false);
-    console.log(gameStart);
+    let [getTriviaData, setGetTriviaData] = React.useState(true);
+    let [triviaData, setTriviaData] = React.useState([]);
 
-    const handleClick = () => {
-        setgameStart(prevGameStart => !prevGameStart);
-
+    const handleClick = (event) => {
+        setgameStart(prevGameStart => !prevGameStart);    
+        if (event.target.textContent === "Start quiz") {
+            setGetTriviaData(prevGetTriviaData => !prevGetTriviaData);
+        }
     }
+
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5")
+            .then(response => response.json())
+            .then(data => setTriviaData(data));
+        
+        console.log(triviaData);
+    }, [getTriviaData])
+
 
     return (
         gameStart === false ? 
