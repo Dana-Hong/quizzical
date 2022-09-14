29dfeece143f41e4f4abb65a70cf08d79a864354
Dana <dana.hong@hotmail.com>
2022-09-14 17:09:11 -0400

    Add more question options in main menu and create labels for them

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -11,19 +11,72 @@ export const MainMenu = (props) => {
             <div className='main-menu-content'>
                 <h1>Quizzical</h1>
                 <h4>An exciting trivia game!</h4>
-                <select
-                    onChange={props.handleOnChange}
-                    name="number-of-questions">
-                        <option value={"5"}>5</option>
-                        <option value={"10"}>10</option>
-                </select>
-                <select
-                    onChange={props.handleOnChange}
-                    name="difficulty">
-                        <option value={"easy"}>Easy</option>
-                        <option value={"medium"}>Medium</option>
-                        <option value={"hard"}>Hard</option>
-                </select>
+                <div className='main-menu-options'>
+                    <div className='main-menu-option'>
+                        <h5 className='option-title'>Number of questions:</h5>
+                        <select
+                            className='option-select'
+                            onChange={props.handleOnChange}
+                            name="number-of-questions">
+                                <option value={"5"}>5</option>
+                                <option value={"10"}>10</option>
+                        </select>
+                    </div>
+                    <div className='main-menu-option'>
+                        <h5 className='option-title'>Category:</h5>
+                        <select
+                            className='option-select'
+                            onChange={props.handleOnChange}
+                            name="number-of-questions">
+                                <option value="9">General Knowledge</option>
+                                <option value="10">Entertainment: Books</option>
+                                <option value="11">Entertainment: Film</option>
+                                <option value="12">Entertainment: Music</option>
+                                <option value="13">Entertainment: Musicals Theatres</option>
+                                <option value="14">Entertainment: Television</option>
+                                <option value="15">Entertainment: Video Games</option>
+                                <option value="16">Entertainment: Board Games</option>
+                                <option value="17">Science Nature</option>
+                                <option value="18">Science: Computers</option>
+                                <option value="19">Science: Mathematics</option>
+                                <option value="20">Mythology</option>
+                                <option value="21">Sports</option>
+                                <option value="22">Geography</option>
+                                <option value="23">History</option>
+                                <option value="24">Politics</option>
+                                <option value="25">Art</option>
+                                <option value="26">Celebrities</option>
+                                <option value="27">Animals</option>
+                                <option value="28">Vehicles</option>
+                                <option value="29">Entertainment: Comics</option>
+                                <option value="30">Science: Gadgets</option>
+                                <option value="31">Entertainment: Japanese Anime Manga</option>
+                                <option value="32">Entertainment: Cartoon Animations</option>
+                        </select>
+                    </div>
+                    <div className='main-menu-option'>
+                        <h5 className='option-title'>Difficulty:</h5>
+                        <select
+                            className='option-select'
+                            onChange={props.handleOnChange}
+                            name="difficulty">
+                                <option value={"easy"}>Easy</option>
+                                <option value={"medium"}>Medium</option>
+                                <option value={"hard"}>Hard</option>
+                        </select>
+                    </div>
+                    <div className='main-menu-option'>
+                        <h5 className='option-title'>Type:</h5>
+                        <select
+                        className='option-select'
+                            onChange={props.handleOnChange}
+                            name="difficulty">
+                                <option value="any">Any Type</option>
+                                <option value="multiple">Multiple Choice</option>
+                                <option value="boolean">True / False</option>
+                        </select>
+                    </div>
+                </div>
                 <button 
                     className='main-menu--start-button' 
                     onClick={props.handleClick}
